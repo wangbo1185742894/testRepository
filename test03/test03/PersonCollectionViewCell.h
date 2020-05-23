4a7e67051d706de8f96d43132249eadc8d7e380f
王博 <wangbo3919@163.com>
2020-05-23 15:43:28 +0800

    添加文件

---
+++ test03/test03/PersonCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  PersonCollectionViewCell.h
+//  test03
+//
+//  Created by 阿兹尔 on 5/23/20.
+//  Copyright © 2020 王博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PersonCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test03/test03/PersonCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PersonCollectionViewCell.m
+//  test03
+//
+//  Created by 阿兹尔 on 5/23/20.
+//  Copyright © 2020 王博. All rights reserved.
+//
+
+#import "PersonCollectionViewCell.h"
+
+@implementation PersonCollectionViewCell
+
+@end
